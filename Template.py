1b948cfc54739846087cc24c350a911ebfb9bb26
Garrett <g.romo2@students.clark.edu>
2020-04-27 12:26:09 -0700

    Doesn't seem to be working, strangely

--- Template.py
+++ Template.py
@@ -12,6 +12,38 @@ Output: return value and description
 """
 
 def main():
-    # code here
+   """
+   A = 2
+   print("\n", A, "\n")
+   a = 22
+   print(a)
+   print()
+
+   AAA = 2
+   print(AAA)
+
+   spam = 222
+   print()
+   print(spam)
+   #print(Spam)
+   print()
+
+   # expression
+   #print(2)
+   print(3 - 1)
+   print((3 - 1) / 2)
+   print(5/3)
+   print(5//3)
+   print(5%3)
+   print(2 + ((3 - 1) / 2))
+   """
+   print()
+   print("The answer is:", 3 + 4, sep="", end="\t")
+   print("end")
+   print()
+
+   for i in range(5):
+       print(i, end="\t")
+    print()
 
 main()    
